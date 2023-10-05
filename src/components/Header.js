49258bc409cdd7c45a39b6da775a2a877636da89
TechPK007 <masumkids1@gmail.com>
2023-10-05 18:05:10 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <div className="header">
+                <img className="logo" src="https://cdn1.iconfinder.com/data/icons/seo-web-17/512/5-512.png" alt=" logo" />
+                <ul className='nav-items'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Services</li>
+                    <li>Blog</li>
+                    <li>Portfolio</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header
